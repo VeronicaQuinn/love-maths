e079fbc4d28a2bbc9b5f468e0d1bec9244f5851f
VeronicaQuinn <veronica.quinn@gmail.com>
2022-02-09 01:39:04 +0000

    Set up structure of game functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,28 @@
 
+function runGame() {
+
+}
+
+function calculateCorrect Answer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+    
+}
+
+function displayMultiplyQuestion() {
+    
+}
